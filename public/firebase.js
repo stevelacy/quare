0618fea93ef605150917fcf00bd7aa31873d9f98
Steve Lacy <me@slacy.me>
2014-08-09 14:29:22 -0700

    purge

--- public/firebase.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  define(function() {
-    return new Firebase('https://energyfuture.firebaseio.com');
-  });
-
-}).call(this);
--- public/pages/Index/Index.view.js
+++ public/pages/Index/Index.view.js
@@ -1,21 +1,22 @@
 (function() {
   define(function(require) {
-    var Dermis, br, button, div, h1, img, span, _ref;
-    _ref = React.DOM, div = _ref.div, h1 = _ref.h1, button = _ref.button, br = _ref.br, span = _ref.span, img = _ref.img;
-    Dermis = require('vendor/dermis');
+    var Dermis, a, br, button, div, h1, img, span, _ref;
+    _ref = React.DOM, div = _ref.div, h1 = _ref.h1, button = _ref.button, br = _ref.br, span = _ref.span, img = _ref.img, a = _ref.a;
+    Dermis = require("vendor/dermis");
     return Dermis.view({
       goNext: function() {
-        return page('/profile');
+        return page("/profile");
       },
       render: function() {
         return div({
           className: "main"
         }, div({
+          className: "button",
           onClick: this.goNext
         }, "Click me"), img({
-          src: '/img/ripple.png'
+          src: "/img/ripple.png"
         }), div({
-          className: 'greytext'
+          className: "greytext"
         }, div({}, "A daily challenge to"), div({}, "change the world, together.")));
       }
     });
